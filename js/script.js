7c8aff01436dc2321bbfb6e32573e038a5d497c0
Jose Cuevas <josecueram@gmail.com>
2019-11-01 12:54:12 -0600

    Refined burger-button and navbar component styles

--- js/script.js
+++ js/script.js
@@ -20,7 +20,16 @@ observer.observe(heroVideo)
 // ? Mobile menu functionaty
 const burgerButton = document.querySelector('#burger-button')
 const navbar = document.querySelector('#navbar')
+const navbarItems = document.querySelectorAll('.c-navbar__item')
+console.log(navbarItems)
 burgerButton.addEventListener('click', () => {
     navbar.classList.toggle('c-navbar--active')
+    navbarItems.forEach((navbarItem, index) => {
+        if (navbarItem.style.animation) {
+            navbarItem.style.animation = ''
+        } else {
+            navbarItem.style.animation = `fade .5s ease forwards ${index / 7 + .3}s`
+        }
+    })
 })
 console.log(burgerButton, navbar)
