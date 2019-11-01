2ec1b06cf95d81fd93446baf290d1bd4095adebd
Jose Cuevas <josecueram@gmail.com>
2019-11-01 14:13:52 -0600

    fixed issue with hero video responsive and mobile

---
+++ js/index-script.js
@@ -0,0 +1,46 @@
+// ? Mobile menu functionaty
+const burgerButton = document.querySelector('#burger-button')
+const navbar = document.querySelector('#navbar')
+const navbarItems = document.querySelectorAll('.c-navbar__item')
+const lines = document.querySelectorAll('.c-burger-button__line')
+
+burgerButton.addEventListener('click', () => {
+    lines.forEach(line => {
+        line.classList.toggle('active')
+    })
+
+    navbar.classList.toggle('c-navbar--active')
+    navbarItems.forEach((navbarItem, index) => {
+        if (navbarItem.style.animation) {
+            navbarItem.style.animation = ''
+        } else {
+            navbarItem.style.animation = `fade .5s ease forwards ${index / 7 + .3}s`
+        }
+    })
+})
+
+const video = document.querySelector('#hero-video')
+const fixed = document.querySelector('.c-fixed')
+var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+if (isMobile) {
+    video.style.display = 'none'
+    fixed.style.backgroundAttachment = 'initial'
+}
+
+// ? Intersection observer for hero video
+const heroVideo = document.querySelector('#hero-video')
+const observer = new IntersectionObserver((entries, observer) => {
+    console.log('video observed!')
+    if (entries[0].isIntersecting) {
+        console.log('play')
+        heroVideo.play()
+    } else {
+        console.log('pause')
+        heroVideo.pause()
+    }
+}, {
+    rootMargin: '300px 0px 0px 0px',
+    threshold: .5,
+})
+
+observer.observe(heroVideo)
--- js/script.js
+++ js/script.js
@@ -18,22 +18,3 @@ burgerButton.addEventListener('click', () => {
         }
     })
 })
-
-// ? Intersection observer for hero video
-const heroVideo = document.querySelector('#hero-video')
-const observer = new IntersectionObserver((entries, observer) => {
-    console.log('video observed!')
-    if (entries[0].isIntersecting) {
-        console.log('play')
-        heroVideo.play()
-    } else {
-        console.log('pause')
-        heroVideo.pause()
-    }
-}, {
-    rootMargin: '300px 0px 0px 0px',
-    threshold: .5,
-})
-
-observer.observe(heroVideo)
-
